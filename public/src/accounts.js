6ca59688cbf306efc3653d8ae2cb8f29632ea8e1
Terra <terra.taylor@gmail.com>
2020-12-16 08:47:23 -0700

    finished first draft of books in possession

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,37 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {
 
-function sortAccountsByLastName(accounts) {}
+  console.log(accounts);
+  console.log(id);
+}
+
+function sortAccountsByLastName(accounts) {
+
+  console.log(accounts);
+}
 
 function numberOfBorrows(account, books) {}
 
-function booksInPossession(account, books, authors) {}
+function booksInPossession(account, books, authors) {
+    let possessedBooks = [];
+
+    for(let i =0; i < books.length;i++){
+      let book = books[i];
+      const {id, title, genre, borrows}=book;
+      for(let j = 0; j< borrows.length; j++){
+        if(borrows[j].id === account.id && borrows[j].returned===false ){
+          for(let k=0; k<authors.length;k++){
+            let author = authors[k];
+            if(author.id ==book.authorId){
+              let tempBook ={ id, title,genre,author, borrows};
+              possessedBooks.push(tempBook);
+            }
+          }
+
+        }
+      }
+    }
+    return possessedBooks;
+}
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -2,9 +2,30 @@ function findAuthorById(authors, id) {}
 
 function findBookById(books, id) {}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  let borrowedBooks =  books.filter(book=> book.borrows[0].returned==false);
+  let returnedBooks =  books.filter(book=>{
+   
+    return book.borrows[0].returned==true;
+})
+ return [borrowedBooks, returnedBooks];
+}
 
-function getBorrowersForBook(book, accounts) {}
+function getBorrowersForBook(book, accounts) {
+  let borrowers =[];
+  accounts.forEach(account=>{
+    book.borrows.forEach(transaction=>{
+        if(transaction.id === account.id){
+          let accountObj = {...account};
+          accountObj.returned= transaction.returned;
+          borrowers.push(accountObj);
+        }
+    })
+  })
+  return borrowers.slice(0,10);
+
+
+}
 
 module.exports = {
   findAuthorById,
